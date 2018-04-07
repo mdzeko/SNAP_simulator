3335e23f94ce5af31fec7b0f0dbefd370850d7c0
Mišo <mdzeko@foi.hr>
2018-04-07 23:04:27 +0200

    Izmjene u pokretanju programa u svrhe testiranja s Nikolom

--- sample.py
+++ sample.py
@@ -1,37 +1,53 @@
+from functools import reduce
+
 from anp.ANP import ANP
 from anp.MatricaUsporedbi import MatricaUsporedbi
 from anp.MatricaZavisnosti import MatricaZavisnosti
 from generator.Generetor import Generator
 from snap.SNAP import SNAP
-
-gen = Generator(1, 4)
-gen.generateAllComparisonMatrices()
-gen.generateDependancyMatrices()
-
-
+brojKlastera = 1
+brojKriterija = 4
+gen = Generator(brojKlastera, brojKriterija)
+gen.generateAllComparisonMatrices(writeToFile=False)
+gen.generateDependancyMatrices(writeToFile=False)
 
 anpList = []
 snapList = []
 
+# fh = open("rez_%d_klust_%d_krit.txt" % (brojKlastera, brojKriterija), "w")
+# fh.write("komb_usporedbe;komb_zavisnosti;konvergira;tezi nuli;cezarova suma;"
+#          "ANP_tezine;SNAP_tezine;Razlika;Postotak_razlika_manjih_od_0.1;"+ '\n')
 # for usporedba in gen.matrices:
-#    for zavisnost in gen.zmatrices:
-#        anp = ANP(usporedba, zavisnost)
-#        anp.simulate()
-#        anpList.append(anp)
-#        snap = SNAP(usporedba, zavisnost)
-#        snap.simulate()
-#        snapList.append(snap)
-        # S.calculateLimitMatrix()
-        # print(S.L)
+#     for zavisnost in gen.zmatrices:
+#         anp = ANP(usporedba, zavisnost)
+#         anp.simulate()
+#         # anpList.append(anp)
+#         snap = SNAP(usporedba, zavisnost)
+#         snap.simulate()
+#         # snapList.append(snap)
+#         razlike = abs(anp.tezine - snap.tezine)
+#         brojManjihOd01 = reduce(lambda x, y: x + (1 if y <= 0.1 else 0), razlike, 0)
+#         fh.write("%s;%s;%d;%d;%d;%s;%s;%s;%d\n" %
+#                  (usporedba.kombinacija, zavisnost.kombinacija,
+#                  anp.supermatrica.konvergira, anp.supermatrica.teziNuli, anp.supermatrica.cezarova,
+#                   anp.tezine, snap.tezine, razlike, (brojManjihOd01/len(snap.tezine))*100))
+#         # S.calculateLimitMatrix()
+#         # print(S.L)
+# fh.close()
+print("Broj kombinacija za %d klastera i %d kriterija: %d" % (brojKlastera, brojKriterija, len(anpList)) )
 
-
-U = MatricaUsporedbi(Generator.izradiMatricu(None, [2, 2, 2, 1, 1, 1], [0.5, 0.5, 0.5, 1, 1, 1], 4))
-Z = MatricaZavisnosti(Generator.izradiMatricu(None, [2, 3, 4, 1, 3, 3], [2, 1, 4, 2, 1, 2], 4, 0))
+U = MatricaUsporedbi(Generator.izradiMatricu(None, [1, 1, 1, 1, 1, 2], [1, 1, 1, 1, 1, 0.5], 4), [1, 1, 1, 1, 1, 2])
+Z = MatricaZavisnosti(Generator.izradiMatricu(None, [0, 0, 0, 0, 0, 1], [1, 1, 1, 1, 1, 1], 4, 0), [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1])
+anp = ANP(U, Z)
+anp.simulate()
 snap = SNAP(U, Z)
 snap.simulate()
+print("Supermatrica\n", anp.supermatrica.S)
+print("Supermatrica - granicna\n", anp.supermatrica.L)
 print("CO", snap.CO)
 print("CI", snap.CI)
 print("CO - CI", snap.razlike)
 print("Norma1", snap.norm)
 print("Norma2", snap.norm2)
-print(snap.tezine)
+print("Tezine", snap.tezine)
+
