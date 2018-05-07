957b4e2c3982adc915a2d3d5ba1e1ee9e57b85ce
Mišo <mdzeko@foi.hr>
2018-05-07 08:42:40 +0200

    Ispravljanje bugova u radu i micanje nepotrebnih parametara iz metoda.
    Pojačan rad sa zapisivanjem u datoteku i maknut nepotreban parametar self iz statičkih metoda.
    TODO: u naziv csv datoteka postaviti i broj kriterija i klastera
    Dodane datoteke s generiranim podacima na git

--- generator/Generetor.py
+++ generator/Generetor.py
@@ -4,6 +4,7 @@ from anp.MatricaZavisnosti import MatricaZavisnosti
 import itertools
 
 
+
 class Generator:
     matrices = []
     zmatrices = []
@@ -22,36 +23,37 @@ class Generator:
     def generateAllComparisonMatrices(self, writeToFile=True):
         NUM_POOL = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8, 1/9]
         if writeToFile:
-            fh = open("usporedbe.txt", "w")
-            fh_prezivjele = open("prezivjele.txt", "w")
+            csvUsporedbe = open("usporedbe.csv", "w")
+            csvPrezivjele = open("prezivjele.csv", "w")
 
         n = self.criteria  # red kvadratne matrice
         br = int((n - 1) * n / 2)  # broj elemenata u gornjem trokutu
         gen_array = list(itertools.combinations_with_replacement(NUM_POOL, br))
         for combination in gen_array:
             a = np.array(list(combination))
-            b = 1 / a
-            U = MatricaUsporedbi(np.matrix(self.izradiMatricu(self, a, b, n)), a)
+            b = np.divide(1., a.astype(float), out=np.zeros_like(a.astype(float)), where=a != 0)
+            # b = 1 / a
+            U = MatricaUsporedbi(np.matrix(self.izradiMatricu(a, b, n)), a)
             if U.relevantna:
                 self.matrices.append(U)
                 if writeToFile:
-                    fh_prezivjele.write(str(a.tolist() + [' konz:', U.konzistentnost]) + '\n')
+                    csvPrezivjele.write(str(a.tolist()) + ";" + str(U.konzistentnost) + "\n")
             if writeToFile:
-                fh.write(str(a.tolist() + [' konz:', U.konzistentnost]) + '\n')
+                csvUsporedbe.write(str(a.tolist()) + ";" + str(U.konzistentnost) + "\n")
         if writeToFile:
-            fh.close()
-            fh_prezivjele.close()
+            csvUsporedbe.close()
+            csvPrezivjele.close()
 
     def generateDependancyMatrices(self, writeToFile=True):
         if writeToFile:
-            fh = open("zavisnosti.txt", "w")
+            fh = open("zavisnosti.csv", "w")
 
         n = self.criteria  # red kvadratne matrice
         br = int((n - 1) * n)  # zbroj broja elemenata u gornjem i donjem trokutu
         gen_array = list(itertools.combinations_with_replacement(range(0, 5), br))
         for combination in gen_array:
             if writeToFile:
-                fh.write(str(list(combination)) + '\n')
+                fh.write(str(list(combination)) + "\n")
             pola = int(br / 2)
             a = np.array(list(combination[:pola]))
             b = np.array(list(combination[pola:]))
@@ -61,18 +63,38 @@ class Generator:
             fh.close()
 
     @staticmethod
-    def izradiMatricu(self, gornji_trokut, donji_trokut, n, dijagonala=1):
+    def izradiMatricu(gornji_trokut, donji_trokut, n, dijagonala=1):
 
         if dijagonala == 0:
             uper = np.zeros((n, n))
         else:
             uper = np.identity(n)
+        try:
+            uper[np.triu_indices(n, 1)] = gornji_trokut
+            indeksi = np.tril_indices(n, -1)
+            uskladi_indekse = list(zip(indeksi[0], indeksi[1]))
+            uskladi_indekse.sort(key=lambda x: x[1])
+            indeksi_DT = (
+                np.array(list(map(lambda x: x[0], uskladi_indekse))), np.array(list(map(lambda x: x[1], uskladi_indekse))))
+            uper[indeksi_DT] = donji_trokut
+        except ValueError:
+            print(gornji_trokut)
+            print(donji_trokut)
+            print(n)
+            print(dijagonala)
 
-        uper[np.triu_indices(n, 1)] = gornji_trokut
-        indeksi = np.tril_indices(n, -1)
-        uskladi_indekse = list(zip(indeksi[0], indeksi[1]))
-        uskladi_indekse.sort(key=lambda x: x[1])
-        indeksi_DT = (
-            np.array(list(map(lambda x: x[0], uskladi_indekse))), np.array(list(map(lambda x: x[1], uskladi_indekse))))
-        uper[indeksi_DT] = donji_trokut
         return uper
+
+    @staticmethod
+    def izradiMatUsporedbe(kombinacija, n):
+        a = np.array(kombinacija)
+        a = a / 1
+        b = np.divide(1., a, out=np.zeros_like(a), where=a != 0.0)
+        return Generator.izradiMatricu(a, b, n)
+
+    @staticmethod
+    def izradiMatZavisnosti(kombinacija, n):
+        pola = int(len(kombinacija) / 2)
+        a = np.array(list(kombinacija[:pola]))
+        b = np.array(list(kombinacija[pola:]))
+        return Generator.izradiMatricu(a, b, n, 0)
